a5b8200d09e55c60592c4abac8d77cd7d696dd02
Václav Kuboň <vaclavkubon@wenceslai-mac.local>
2023-09-09 19:37:57 +0200

    changing method of scheduling

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 import schedule
 import time
+from fastapi_utils.tasks import repeat_every
 
 from email_parsing import parse_unread_emails
 
@@ -8,7 +9,10 @@ app = FastAPI()
 
 
 @app.on_event("startup")
+@repeat_every(seconds=60 * 60 * 24)  # repeat every 24h
 def initialization():
+    print("executing periodic reading of emails")
+    """
     schedule.every().day.at("00:00").do(parse_unread_emails)
 
     # Run the scheduler continuously.
@@ -16,6 +20,7 @@ def initialization():
         schedule.run_pending()
         time.sleep(1)  # Sleep for 10 second to avoid high CPU usage
 
+    """
 
 @app.get("/")
 async def root():
